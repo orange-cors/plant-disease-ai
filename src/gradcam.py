c9cd05524e2bdcd1a20f90b459f64c42d415ad02
orange-cors <mango.cors@gmail.com>
2026-08-10 22:34:55 +0700

    plantdes

--- src/gradcam.py
+++ src/gradcam.py
@@ -14,7 +14,7 @@ def visualize_gradcam(model, image_path, true_label=None):
     print(f"Analyzing image: {image_path}")
 
     transform = T.Compose([
-        T.Resize(299),              
+        T.Resize(256),              
         T.CenterCrop(224),           
         T.ToTensor(),               
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
